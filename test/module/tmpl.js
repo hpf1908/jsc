5ec7707ac3e97b776aca05c7e34df152f325f2dc
hpf1908 <hpf1908@gmail.com>
2012-06-14 02:59:00 +0800

    add html parser

---
+++ test/module/tmpl.js
@@ -0,0 +1,32 @@
+define("./tmpl",[],function(require, exports, module){
+var tmpl = { 
+"a_tmpl":function(data){
+
+var __p=[],_p=function(s){__p.push(s)};
+__p.push('<script type="text/html" id="a_tmpl">\
+ 		<div>hello world</div>\
+ 		');
+ for(var i = 0; i < 5; i++){__p.push(' 			');
+_p(i);
+__p.push(' 		');
+}__p.push(' 	</script>');
+
+return __p.join("");
+},
+
+"b_tmpl":function(data){
+
+var __p=[],_p=function(s){__p.push(s)};
+__p.push('<script type="text/html" id="b_tmpl">\
+ 		<div>hello world</div>\
+ 		');
+ for(var i = 0; i < 5; i++){__p.push(' 			');
+_p(i);
+__p.push(' 		');
+}__p.push(' 	</script>');
+
+return __p.join("");
+}
+};
+return tmpl;
+});
--- test/parser.js
+++ test/parser.js
@@ -1,7 +1,9 @@
-var jsc      = require('../lib/jsc'),
-	jsParser = require('../lib/parser/js'),
-	fs		 = require('fs'),
-	should   = require('should');
+var jsc      	= require('../lib/jsc'),
+	jsParser 	= require('../lib/parser/js'),
+	htmlParser 	= require('../lib/parser/html'),
+	fs		 	= require('fs'),
+	util	 	= require('../lib/util'),
+	should   	= require('should');
 
 module.exports = {
 
@@ -26,10 +28,56 @@ module.exports = {
 		var path = __dirname + '/module/src';
 		var output = __dirname + '/module/js.js';
 		jsc.createJs(path , output);
+	},
+	'should splite string by reg' : function(){
+
+		var regTmpl = /<script([^<>]+?)type="text\/html"([\w\W\r\n]*?)>(?:\r\n|\r|\n)?([\w\W\r\n]*?)(?:\r\n|\r|\n)?<\/script>/gmi;
+		var inputStr = '<html><script type="text/html" id="a_tmpl">' +
+							'<div>hello world</div>'+
+						'</script></html>';
+
+		var outputStr = util.analyzeStringByReg(inputStr , regTmpl, {
+			merge : true,
+			parseHandler : function(str , exec){
+				return 'test';
+			}
+		});
+
+		var outputCorrectStr = '<html>test</html>';
+
+		outputStr.should.equal(outputCorrectStr);
+
+	},
+	'should parse tmpl to js template' : function(){
+
+		var inputStr = fs.readFileSync('./module/src/a.page.tmpl' ,'UTF-8');
+		var outputStr = htmlParser.parseTmpl(inputStr);
+
+		//console.log(outputStr);
+	},
+	'should parse tmpl widh folder' : function(){
+		var path = __dirname + '/module/src';
+		var outputStr = htmlParser.parse(path);
+
+		//console.log(outputStr);
+	},
+	'should create tmpl widh folder' : function(){
+		var path = __dirname + '/module/src';
+		var output = __dirname + '/module/tmpl.js';
+		jsc.createTmpl(path , output , {
+			mname : './tmpl'
+		});
 	}
 }
 
-//module.exports['should parse module add name and deps']();
-//module.exports['should parse js by given path']();
+/*
+module.exports['should parse module add name and deps']();
+module.exports['should parse js by given path']();
 module.exports['should create js by given path']();
+module.exports['should splite string by reg']();
+module.exports['should parse tmpl to js template']();
+module.exports['should parse tmpl widh folder']();
+module.exports['should create tmpl widh folder']();
+*/
+
 
